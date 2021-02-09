0e6f14bc6542ec7c14f4ec81796364a36343e74e
FranklinsHand <alex.pearson2000@gmail.com>
2021-02-09 16:45:00 -0500

    updated .gitignore

---
+++ .eleventy.js
@@ -0,0 +1,43 @@
+
+
+const fs = require("fs");
+
+const src = require("./_src/")
+
+module.exports =  function(eleventyConfig) {
+
+  //exposes eleventyConfig function to the 'includes folder'
+  src(eleventyConfig)
+
+  eleventyConfig.setBrowserSyncConfig({//code to make 404 page work. I have no idea how it works tho lol so don't touch it
+    callbacks: {
+      ready: function(err, bs) {
+
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync('_site/404.html');
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
+
+  return {
+    dir: {
+      input: ".",
+      includes: "",
+      layouts: "_layouts",
+      output: "_site",
+      data: "_data"
+    },
+    templateFormats: [
+      'html',
+      'md',
+      '11ty.js'
+    ],
+  };
+
+}
